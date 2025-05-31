27b2f682a366bee24546414750bfd343276d66e5
senn59 <74777070+senn59@users.noreply.github.com>
2025-05-31 15:45:37 +0200

    feat: basic tab switching functionality and async iife

--- background.js
+++ background.js
@@ -1,6 +1,14 @@
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "queryTabs") {
         return browser.tabs.query({})
-            .then(tabs => tabs);
+            .then(tabs => tabs.map(t => { //.filter(t => !t.active)
+                return {
+                    id: t.id,
+                    title: t.title,
+                    url: t.url,
+                }
+            }));
+    } else if (message.action === "switchTab") {
+        browser.tabs.update(message.tab.id, {active: true});
     }
 });
--- main.js
+++ main.js
@@ -1,19 +1,32 @@
-console.log("Loaded keyboard-tab-switcher");
 
-function logTabs(tabs) {
-  console.log(tabs);
-}
-
-browser.runtime.sendMessage({ action: "queryTabs" }).then(tabs => {
-    console.log(tabs)
-})
-
-const menu = browser.runtime.getURL("menu.html");
-fetch(menu)
-    .then(response => response.text())
-    .then(html => {
+const loadMenu = async (url) => {
+    try {
+        const response = await fetch(url);
+        const html = await response.text();
         const container = document.createElement("div");
         container.innerHTML = html;
-        document.body.appendChild(container); 
-    })
-    .catch(err => console.error("Error loading keyboard-tab-switcher menu", err));
+        document.body.appendChild(container);
+        return container
+    } catch (err) {
+        console.error("Error loading keyboard-tab-switcher menu", err)
+        throw err
+    }
+}
+
+(async () => {
+    console.log("Loaded keyboard-tab-switcher");
+    const menu = await loadMenu(browser.runtime.getURL("menu.html"));
+
+    const tabsContainer = menu.querySelector("#tabs-cnt")
+    const tabs = await browser.runtime.sendMessage({ action: "queryTabs" })
+    tabs.forEach(t => {
+        const el = document.createElement("div");
+        el.innerText = t.title;
+        el.onclick = () => {
+            browser.runtime.sendMessage({ action: "switchTab", tab: t })
+        }
+        tabsContainer.append(el);
+    });
+
+})();
+

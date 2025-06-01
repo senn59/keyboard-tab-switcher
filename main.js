55aa409c78c4c32c7ef33137cba76bf8d76fd335
senn59 <74777070+senn59@users.noreply.github.com>
2025-06-01 11:22:29 +0200

    chore: handle event listeners better, add search event listener

--- main.js
+++ main.js
@@ -42,12 +42,13 @@ const loadTabs = async (tabsContainer) => {
         tabItem.append(tabTitle);
         tabsContainer.append(tabItem);
     });
-    tabsContainer.firstChild.ariaSelected = true;
-    selectedTab = tabsContainer.firstChild;
+    setSelectedTab(tabsContainer.firstChild);
 }
 
 const setSelectedTab = (element) => {
-    selectedTab.ariaSelected = false;
+    if (selectedTab) {
+        selectedTab.ariaSelected = false;
+    }
     selectedTab = element;
     element.ariaSelected = true;
 }
@@ -60,7 +61,11 @@ const commandHandler = async (cmd) => {
                 menu = await loadMenu();
                 tabsContainer = menu.shadowRoot.querySelector("#tabs-cnt");
                 searchBar = menu.shadowRoot.querySelector("#search-bar");
+                console.log(searchBar)
                 searchBar.focus();
+                searchBar.addEventListener("input", handleSearch);
+                window.addEventListener("click", handleClick);
+                document.addEventListener("keydown", handleKeydown)
             }
             await loadTabs(tabsContainer);
             break;
@@ -70,6 +75,10 @@ const commandHandler = async (cmd) => {
             }
             menu.remove();
             menu = undefined;
+
+            document.removeEventListener("keydown", handleKeydown);
+            window.removeEventListener("click", handleClick);
+            searchBar.removeEventListener("change", handleSearch);
             break;
         case "cycle-tab":
             if (selectedTab.nextSibling) {
@@ -99,31 +108,34 @@ const commandHandler = async (cmd) => {
  * Keys like 'Escape', 'Tab', 'Enter' aren't allowed as browser commands.
  * We add an event listener to work around this.
 */
-document.addEventListener("keydown", (event) => {
-    if (menu) {
-        switch (event.key) {
-            case "Escape":
-                event.preventDefault()
-                commandHandler({ action: "close-switcher" })
-                break;
-            case "Tab":
-                event.preventDefault();
-                const command = event.shiftKey ? "cycle-tab-reverse" : "cycle-tab";
-                commandHandler({ action: command });
-                break;
-            case "Enter":
-                event.preventDefault();
-                commandHandler({ action: "switch-tab" });
-                break;
-        }
-    }
-})
 
-window.addEventListener('click', function(event) {
+const handleKeydown = (event) => {
+    switch (event.key) {
+        case "Escape":
+            event.preventDefault()
+            commandHandler({ action: "close-switcher" })
+            break;
+        case "Tab":
+            event.preventDefault();
+            const command = event.shiftKey ? "cycle-tab-reverse" : "cycle-tab";
+            commandHandler({ action: command });
+            break;
+        case "Enter":
+            event.preventDefault();
+            commandHandler({ action: "switch-tab" });
+            break;
+    }
+}
+
+const handleClick = (event) => {
     if (menu && !menu.contains(event.target)) {
         commandHandler({ action: "close-switcher" });
     }
-});
+}
+
+const handleSearch = (e) => {
+    console.log(e.target.value);
+}
 
 browser.runtime.onMessage.addListener(commandHandler);
 
